57e0315a2a8bf92311f4ab0affc79291abc1cf1b
fajrwajr <bookreaderfajr@gmail.com>
2021-10-08 23:12:50 +0000

    Moved Project

--- src/app.js
+++ src/app.js
@@ -1,11 +1,24 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+const DomainCreate = {
+  pronoun: ["the", "our"],
+  adj: ["great", "big"],
+  noun: ["jogger", "racoon"]
 };
+
+let extension = [".com", ".net", ".gov", ".us"];
+function createDomainGenerator(extension) {
+  let newArr = "";
+  DomainCreate.pronoun.forEach(function(pronoun) {
+    DomainCreate.adj.forEach(function(adj) {
+      DomainCreate.noun.forEach(function(noun) {
+        let result = "<p>" + pronoun + adj + noun + extension + "</p>";
+        newArr += result + "\n";
+      });
+    });
+  });
+  document.querySelector("#demo").innerHTML = newArr;
+}
+createDomainGenerator(extension[Math.floor(Math.random() * 4)]);
+document.querySelector("#refresh").addEventListener("click", () => {
+  let extArr = Math.floor(Math.random() * 4);
+  createDomainGenerator(extension[extArr]);
+});
